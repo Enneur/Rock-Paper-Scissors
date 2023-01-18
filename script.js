ce7d89ed80a21ff688a7bbcd5a95e6228e3ad1cb
Enneur <abden.idrissi@gmail.com>
2023-01-18 11:48:19 +0100

    create a 5 round vs computer function that tells u if you win, lose or draw

--- script.js
+++ script.js
@@ -1,31 +1,45 @@
 function getComputerChoice() {
+
     const choice = ["Rock", "Paper", "Scissors"];
     let rNumber = Math.floor(Math.random() * 3);
     return choice[rNumber];
 
 }
 
+let win = 0 ;
+let lose = 0 ;
 
 function playRound(playerSelection, computerSelection) {
-
     if (playerSelection.toUpperCase() === "PAPER") {
         if (computerSelection.toUpperCase() === "ROCK") {
-            return "You Win! Paper beats Rock.";
+            console.log("You Win! Paper beats Rock.");
+            win = win + 1;
+            return win;
         } else if (computerSelection.toUpperCase() === "SCISSORS") {
-            return "You lose! Scissors beats Paper.";
+            console.log("You lose! Scissors beats Paper.");
+            lose = lose + 1;
+            return lose;
         }
     } else if (playerSelection.toUpperCase() === "ROCK") {
         if (computerSelection.toUpperCase() === "SCISSORS") {
-            return "You Win! Rock beats Scissors.";
+            console.log("You Win! Paper beats Rock.");
+            win = win + 1;
+            return win;
         } else if (computerSelection.toUpperCase() === "PAPER") {
-            return "You lose! Paper beats Rock.";
+            console.log("You lose! Scissors beats Paper.");
+            lose = lose + 1;
+            return lose;
         }
 
     } else if (playerSelection.toUpperCase() === "SCISSORS") {
         if (computerSelection.toUpperCase() === "PAPER") {
-            return "You Win! Scissors beats Paper.";
+            console.log("You Win! Paper beats Rock.");
+            win = win + 1;
+            return win;
         } else if (computerSelection.toUpperCase() === "ROCK") {
-            return "You lose! Rock beats Scissors.";
+            console.log("You lose! Scissors beats Paper.");
+            lose = lose + 1;
+            return lose;
         }
     }
 
@@ -34,7 +48,24 @@ function playRound(playerSelection, computerSelection) {
     }
 }
 
-const playerSelection = "Scissors";
-const computerSelection = getComputerChoice();
+function game() {
 
-console.log(playRound(playerSelection, computerSelection));
+    for (let i = 0; i < 5; i++) {
+        let round = prompt("Choose Rock Paper or Scissors.");
+        const computer = getComputerChoice();
+        console.log(playRound(round, computer));
+    }
+
+    console.log(win);
+    console.log(lose);
+    if (win > lose) {
+        alert("You won the Game!")
+    } else if (win < lose) {
+        alert("You lost the Game!")
+    } else {
+        alert("Draw!");
+    }
+    
+}
+
+console.log(game());
